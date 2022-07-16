ee3ebba69bfa43a768124870b2c790bc1cf02044
kevin <wanjunfeng@gmail.com>
2022-07-16 14:20:04 +0800

    initial import

---
+++ gateway.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/gateway"
+)
+
+var configFile = flag.String("f", "config.yaml", "config file")
+
+func main() {
+	flag.Parse()
+	logx.DisableStat()
+
+	var c gateway.GatewayConf
+	conf.MustLoad(*configFile, &c)
+	gw := gateway.MustNewServer(c)
+	defer gw.Stop()
+	gw.Start()
+}
